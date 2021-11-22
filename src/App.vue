f856acdb3b02aaa89b560251457f463cad5824e6
Navi <mailvoornavi@gmail.com>
2021-11-22 02:13:37 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<navigation />
+	<router-view />
+</template>
+
+<script>
+	import Navigation from './components/Navigation.vue'
+
+	export default {
+		name: 'App',
+
+		components: {
+			Navigation
+		}
+	}
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+	<ul>
+		<li>
+			<router-link :to="{ name: 'home' }">
+				Home
+			</router-link>
+		</li>
+		<li>
+			<router-link :to="{ name: 'test' }">
+				Test
+			</router-link>
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		name: 'Navigation',
+	}
+</script>
