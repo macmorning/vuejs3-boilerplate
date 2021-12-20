4f3df2b570f671d68038a46ee41330eb13dea04a
Naviisml <43450192+naviisml@users.noreply.github.com>
2021-12-20 19:33:06 +0100

    added router class & `routes` folder

---
+++ src/js/router.js
@@ -0,0 +1,100 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+class Router
+{
+	/**
+	 * The constructor
+	 */
+	constructor(config)
+	{
+		this.resolveRoutes(
+			import.meta.globEager('./routes/*.js')
+		)
+		this.createRouterInstance(config)
+	}
+
+	/**
+	 * Create a new router instance
+	 *
+	 * @param   {Object}  config
+	 *
+	 * @return  {Router}
+	 */
+	createRouterInstance(config)
+	{
+		let routes = this.routes
+
+		this.router = createRouter({
+			history: createWebHistory(),
+			routes
+		})
+
+		this.router.beforeEach((to, from, next) => this.beforeEach(to, from, next))
+		this.router.afterEach((to, from) => this.afterEach(to, from))
+
+		return this.router
+	}
+
+	/**
+	 * The beforeEach function for each route
+	 *
+	 * @param   {Object}  to
+	 * @param   {Object}  from
+	 * @param   {void}  next
+	 *
+	 * @return  {void}
+	 */
+	async beforeEach (to, from, next) {
+		// TODO: Look for the components (to.matched),
+		// and resolve the guards we wanna execute before
+		// visiting this page.
+
+		// TODO: this.router.$app.setLayout(to.meta.layout)
+
+		next()
+	}
+
+	/**
+	 * The afterEach function for each route
+	 *
+	 * @param   {Object}  to
+	 * @param   {Object}  from
+	 *
+	 * @return  {void}
+	 */
+	async afterEach (to, from) {
+		//...
+	}
+
+	/**
+	 * Resolve the route files
+	 *
+	 * @param   {Object}  files
+	 *
+	 * @return  {Array}
+	 */
+	resolveRoutes(files)
+	{
+		this.routes = this.routes ?? []
+		
+		for (var key in files) {
+			if (files.hasOwnProperty(key)) {
+				this.routes.push(...files[key].default)
+			}
+		}
+
+		return this.routes	
+	}
+
+	/**
+	 * Return the router instance
+	 *
+	 * @return  {Router}
+	 */
+	getRouterInstance()
+	{
+		return this.router
+	}
+}
+
+export const router = new Router({})
