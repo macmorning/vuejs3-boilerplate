81cc9a90daec9e43aec4c8a183129233f9f2ddc6
Navi <mailvoornavi@gmail.com>
2021-12-30 18:11:41 +0100

    added (unfinished) example of layouts

---
+++ src/js/pages/Example.vue
@@ -0,0 +1,15 @@
+<template>
+	Example
+</template>
+
+<script>
+	export default {
+		name: 'Example',
+
+		created () {
+			// Force change a layout
+			// TODO: Make router.beforeEach get and set the layout
+			this.$root.setLayout('Example')
+		},
+	}
+</script>
